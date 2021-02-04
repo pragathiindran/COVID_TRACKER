2f44f75d4508006fbdcf8ddea6165d27b2675af2
pragathiindran <50520367+pragathiindran@users.noreply.github.com>
2021-02-04 08:10:32 +0530

    Add files via upload

---
+++ build/precache-manifest.279995b1dabb66aa6f597046e16715b1.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5623084369fa0a35147617ae57bd2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25d4dcd3821e4700ab6",
+    "url": "/static/css/4.a653b8e5.chunk.css"
+  },
+  {
+    "revision": "22cf80299f5d1c2eace0",
+    "url": "/static/css/5.5764820c.chunk.css"
+  },
+  {
+    "revision": "9901ce90db7112cb4e4b",
+    "url": "/static/css/7.0bd78202.chunk.css"
+  },
+  {
+    "revision": "eb6972ab0edf16d8354c",
+    "url": "/static/css/8.d79eed79.chunk.css"
+  },
+  {
+    "revision": "8553872ffbfea380f257",
+    "url": "/static/css/main.d4951c55.chunk.css"
+  },
+  {
+    "revision": "891b69d8f2fda6693028",
+    "url": "/static/js/0.1044b1ab.chunk.js"
+  },
+  {
+    "revision": "03fef662697e8ab35284",
+    "url": "/static/js/10.2969a328.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/10.2969a328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b5474c1dcba3aacfe3",
+    "url": "/static/js/3.89d1278a.chunk.js"
+  },
+  {
+    "revision": "00ac245e1ed65272ee7eee902b8de8a1",
+    "url": "/static/js/3.89d1278a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25d4dcd3821e4700ab6",
+    "url": "/static/js/4.e83d50ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/4.e83d50ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cf80299f5d1c2eace0",
+    "url": "/static/js/5.dcaa0838.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/5.dcaa0838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0beede4c7422d2a116",
+    "url": "/static/js/6.e7249908.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/static/js/6.e7249908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9901ce90db7112cb4e4b",
+    "url": "/static/js/7.7d0ef9c5.chunk.js"
+  },
+  {
+    "revision": "eb6972ab0edf16d8354c",
+    "url": "/static/js/8.333b2243.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/8.333b2243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663c7cfe0bfdaa59eed0",
+    "url": "/static/js/9.66611860.chunk.js"
+  },
+  {
+    "revision": "8553872ffbfea380f257",
+    "url": "/static/js/main.c9b868b4.chunk.js"
+  },
+  {
+    "revision": "2ed8ea22729b264a0975",
+    "url": "/static/js/runtime-main.b0e3c2ce.js"
+  },
+  {
+    "revision": "ccd628f5306acb680b4c67878005a54f",
+    "url": "/static/media/NotoSans-Bold.ccd628f5.ttf"
+  },
+  {
+    "revision": "300942338fa9a1d5372f66a70a2d8a78",
+    "url": "/static/media/avoidContact.30094233.jpg"
+  },
+  {
+    "revision": "49d3939c2cb059c41199a964abba9d5e",
+    "url": "/static/media/breathing.49d3939c.jpg"
+  },
+  {
+    "revision": "efd74a8d67668e97d227f5d5a4ee47a2",
+    "url": "/static/media/chills.efd74a8d.jpg"
+  },
+  {
+    "revision": "1df3055894905ecc3b1e7e452cb273e6",
+    "url": "/static/media/cold.1df30558.png"
+  },
+  {
+    "revision": "e0f3f9fed76a7f2f0bb6ae2748315a67",
+    "url": "/static/media/cough.e0f3f9fe.jpg"
+  },
+  {
+    "revision": "bfe309fd4f7e188cf66de1fa26014ef9",
+    "url": "/static/media/covid.bfe309fd.jpg"
+  },
+  {
+    "revision": "1eb81a256e4c483a771f20ea1fdb646c",
+    "url": "/static/media/diarrhea.1eb81a25.jpg"
+  },
+  {
+    "revision": "b881ae0b750ae7f36dd91f7ab2522db6",
+    "url": "/static/media/fatigue.b881ae0b.jpg"
+  },
+  {
+    "revision": "9e2935ca5ca8ec794c0c7ea27f2ea22d",
+    "url": "/static/media/fever.9e2935ca.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4511918ec3046643a7d15ed4e8b37240",
+    "url": "/static/media/headaches.4511918e.png"
+  },
+  {
+    "revision": "06e0c33975f307e0e0865c79c3286f01",
+    "url": "/static/media/mask.06e0c339.jpg"
+  },
+  {
+    "revision": "bb8c6c55f8db97483388f71c29a01d9f",
+    "url": "/static/media/naussea.bb8c6c55.jpg"
+  },
+  {
+    "revision": "0825b5b8d3339667b82dc9ecafe4066a",
+    "url": "/static/media/stayHome.0825b5b8.jpg"
+  },
+  {
+    "revision": "780acfd19f1479740eb4ec00fbb02652",
+    "url": "/static/media/stayInformed.780acfd1.png"
+  },
+  {
+    "revision": "7c9fae1b8c535f78770abeec58e180e5",
+    "url": "/static/media/washHands.7c9fae1b.png"
+  }
+]);
